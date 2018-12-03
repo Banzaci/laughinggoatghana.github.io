44b7726980ffc46b069b5b70e385f7555e1d37f4
Michael Larsson <michedotab@gmail.com>
2018-12-03 11:50:09 +0100

    T

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   assetPrefix: isProd ? 'https://laughinggoatghana.com' : '',
-  'process.env.BACKEND_URL': isProd ? 'https://laughinggoatghana.com' : '',
   exportPathMap: async function (defaultPathMap) {
     return {
       '/': {page:'/'},
