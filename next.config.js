5f5e6a183ea5fd5a450a232102fb1e72cff5f377
Michael Larsson <michedotab@gmail.com>
2018-12-04 16:00:31 +0100

    das

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const webpack = require('webpack')
+// const webpack = require('webpack')
 const isProd = process.env.NODE_ENV === 'production'
 const assetPrefix = isProd ? '/laughinggoatghana.github.io' : ''
 module.exports = {
--- temp.js
+++ temp.js
@@ -1,1 +1,3 @@
 /* --cleanup --verbose --remote=master */
+
+git push origin `git subtree split --prefix out gh-pages` :gh-pages --force
