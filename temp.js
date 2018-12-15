b1a95488139209c6872cec42ce17c5857c20eecb
Michael Larsson <michedotab@gmail.com>
2018-12-15 16:06:43 +0100

    Remove mock

--- temp.js
+++ temp.js
@@ -2,4 +2,14 @@
 
 https://dev.to/letsbsocial1/deploying-to-gh-pages-with-git-subtree
 
+git checkout -b gh-pages
+
+git rm -rf .
+
+git commit -am "First commit to gh-pages branch"
+
+git push origin gh-pages
+
+git checkout master
+
 git push origin `git subtree split --prefix out gh-pages`:gh-pages --force
