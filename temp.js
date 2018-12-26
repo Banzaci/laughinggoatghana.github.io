42e45ba48581fbf0221daa1f6430de8fac0535ac
Michael Larsson <michedotab@gmail.com>
2018-12-26 08:32:40 +0100

    Add to temp

--- temp.js
+++ temp.js
@@ -5,6 +5,7 @@ https://dev.to/letsbsocial1/deploying-to-gh-pages-with-git-subtree
 next build
 next export
 git branch -d gh-pages
+git push origin --delete gh-pages
 git checkout -b gh-pages
 git rm -rf .
 git commit -am "First commit to gh-pages branch"
