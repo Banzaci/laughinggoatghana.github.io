1eb23335db9cb08aa6f2a84ea9c45bfa53cabc3d
Michael Larsson <michedotab@gmail.com>
2018-12-26 08:29:49 +0100

    New to temp

--- temp.js
+++ temp.js
@@ -2,14 +2,12 @@
 
 https://dev.to/letsbsocial1/deploying-to-gh-pages-with-git-subtree
 
+next build
+next export
+git branch -d gh-pages
 git checkout -b gh-pages
-
 git rm -rf .
-
 git commit -am "First commit to gh-pages branch"
-
 git push origin gh-pages
-
 git checkout master
-
 git push origin `git subtree split --prefix out gh-pages`:gh-pages --force
